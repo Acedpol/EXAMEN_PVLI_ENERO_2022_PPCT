a0b8fa96e04e4eebec29165335c25097fc71424e
ACE D. POL <pablocub@ucm.es>
2022-01-12 00:50:34 +0100

    Ajustado correctamente al contenedor HTML

--- examen-pvli/src/game.js
+++ examen-pvli/src/game.js
@@ -5,11 +5,12 @@ window.onload = ()=>{
 
     const config = {
         type: Phaser.AUTO,
+        parent: "phaserGame",
         scale: {
             width: 256,
             height: 192,
             zoom: 3,
-            autoCenter: Phaser.Scale.Center.CENTER_HORIZONTALLY
+            // autoCenter: Phaser.Scale.Center.CENTER_HORIZONTALLY
         },
         pixelArt: true,
         scene: [ Boot, Jetpac ]
@@ -17,30 +18,3 @@ window.onload = ()=>{
 
     new Phaser.Game(config);
 };
-
-export class JetPacGame extends Phaser.Game {
-    constructor(config_aves) {
-        super(config_aves, { key:'game' });
-    }
-
-    preload() {
-        
-    }
-
-    create() {
-        // main settings:
-        game.world.setBounds(0, 0, 5000, 600);
-        game.physics.startSystem(Phaser.Physics.ARCADE);
-        game.physics.arcade.checkCollision.up = false; // colision con el techo
-        game.physics.arcade.checkCollision.down = false; // colision con el suelo
-
-        this.scene.start('boot');
-
-    }
-
-    update() {
-
-    }
-}
-
-
