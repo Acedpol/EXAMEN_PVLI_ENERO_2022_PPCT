64ef4cb56b9743aba9eab59d79d080c71712684f
Pablo Cubells <pablocub@ucm.es>
2022-01-11 10:29:00 +0100

    apenas he empezado... tras una hora...

--- examen-pvli/src/game.js
+++ examen-pvli/src/game.js
@@ -1,3 +1,5 @@
+import Boot from './boot.js';
+import Jetpac from './jetpac.js';
 
 window.onload = ()=>{
 
@@ -10,8 +12,35 @@ window.onload = ()=>{
             autoCenter: Phaser.Scale.Center.CENTER_HORIZONTALLY
         },
         pixelArt: true,
-        scene: [  ]
+        scene: [ Boot, Jetpac ]
     };
 
     new Phaser.Game(config);
 };
+
+export class JetPacGame extends Phaser.Game {
+    constructor(config_aves) {
+        super(config_aves, { key:'game' });
+    }
+
+    preload() {
+        
+    }
+
+    create() {
+        // main settings:
+        game.world.setBounds(0, 0, 5000, 600);
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.physics.arcade.checkCollision.up = false; // colision con el techo
+        game.physics.arcade.checkCollision.down = false; // colision con el suelo
+
+        this.scene.start('boot');
+
+    }
+
+    update() {
+
+    }
+}
+
+
